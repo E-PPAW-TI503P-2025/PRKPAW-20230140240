b9e8977cac403674d263655cca280b8fcb0334bf
taznimt <taznimanwar06@gmail.com>
2025-10-03 09:55:55 +0700

    first commit

---
+++ 20230140240-react/src/App.js
@@ -0,0 +1,22 @@
+// src/App.js
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000') // request ke server Node.js
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Integrasi React dan Node.js</h1>
+      <p>Pesan dari server: <strong>{message}</strong></p>
+    </div>
+  );
+}
+
+export default App;
